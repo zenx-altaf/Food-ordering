cbe2f8c115319fac7b20e120fdf236e99d749d57
Altaf <altaf11mohd@gmail.com>
2022-05-09 01:44:54 +0530

    ghj

---
+++ src/Components/BannerName.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function BannerName({ name, discount, more }) {
+  const currency = "₹";
+  return (
+    <div className="bannerContent">
+      <h3>Hello FOODIE,</h3>
+      <p>
+        Get free discount for every <span>{`${currency}${discount}`}</span>{" "}
+        purchase
+      </p>
+      <a href={more}>Learn More</a>
+    </div>
+  );
+}
+
+export default BannerName;
